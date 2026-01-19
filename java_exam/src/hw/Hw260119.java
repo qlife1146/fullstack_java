90911393bd3a4542ea9cbfa62e49aae6cadeb332
Luca Park <qlife1146@gmail.com>
2026-01-19 17:50:54 +0900

    주석 및 범위 수정

--- java_exam/src/hw/Hw260119.java
+++ java_exam/src/hw/Hw260119.java
@@ -2,9 +2,18 @@ package hw;
 
 public class Hw260119 {
   public static void main(String[] args) {
-    for (int i = 2; i * i <= 1000000; i += 1) {
-      if (100000 % i == 0) {
-        System.out.println(100000 % i);
+    // 소수: 약수가 1과 본인 뿐인 수
+    for (int i = 2; i <= 100000; i += 1) {
+      int count = 0;
+      for (int j = 2; j * j <= i; j += 1) {
+        if (i % j == 0) {
+          // 본인 외 다른 수와 나눠지면 count 상승 = 소수 아님
+          count = count + 1;
+        }
+      }
+      if (count == 0) {
+        // count가 0일 때 = 소수일 때만 해당 수 출력
+        System.out.println(i);
       }
     }
   }
